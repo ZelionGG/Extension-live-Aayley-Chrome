e01ffab898783c220efcfc0093ca041fb00cd9a0
Aurélien ALVES <contact@zeliongg.com>
2020-05-14 18:03:45 +0200

    Commit de la nouvelle version avec passage sur l'API Kraken.

--- parameters.js
+++ parameters.js
@@ -1,1 +1,14 @@
-var aayley_params = {"title":"Aayley","message":"Aayley est en live","offlineUrl":"http:\/\/www.twitch.tv\/aayley","chaines":[{"id":"aayley","type":"twitch","redirect":"http:\/\/www.twitch.tv\/aayley","key":""}]}; if (typeof require !== "undefined") {exports.aayley_params = aayley_params;}
+var aayley_params = {
+    "title": "Aayley",
+    "message": "Aayley est en live",
+    "offlineUrl": "http:\/\/www.twitch.tv\/aayley",
+    "chaines": [{
+        "id": "71356558",
+        "type": "twitch",
+        "redirect": "http:\/\/www.twitch.tv\/aayley",
+        "key": ""
+    }]
+};
+if (typeof require !== "undefined") {
+    exports.aayley_params = aayley_params;
+}
